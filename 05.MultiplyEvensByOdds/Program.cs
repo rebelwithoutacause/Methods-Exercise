5545d25d7dff76c4dc816040dd51be6df6f72d5a
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-03 22:02:51 +0200

    Add files via upload

---
+++ 05.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace MultiplyEvenAndOddDigits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            int number = int.Parse(Console.ReadLine());
+            int result = GetMultipleOfEvenAndOdds(number);
+            Console.WriteLine(result);
+        }
+
+        
+        static int GetMultipleOfEvenAndOdds(int number)
+        {
+            int sumEven = GetSumOfEvenDigits(number);
+            int sumOdd = GetSumOfOddDigits(number);
+
+            
+            return sumEven * sumOdd;
+        }
+
+         
+        static int GetSumOfEvenDigits(int number)
+        {
+            int sumEven = 0;
+             
+            number = Math.Abs(number);
+
+             
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 == 0) 
+                {
+                    sumEven += digit;
+                }
+                number /= 10;  
+            }
+
+            return sumEven;
+        }
+
+         
+        static int GetSumOfOddDigits(int number)
+        {
+            int sumOdd = 0;
+            
+            number = Math.Abs(number);
+
+             
+            while (number > 0)
+            {
+                int digit = number % 10;
+                if (digit % 2 != 0)  
+                {
+                    sumOdd += digit;
+                }
+                number /= 10; 
+            }
+
+            return sumOdd;
+        }
+    }
+}
---
+++ 06.Orders/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ProductOrder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            string product = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+             
+            double totalPrice = CalculateTotalPrice(product, quantity);
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+
+         
+        static double CalculateTotalPrice(string product, int quantity)
+        {
+            double pricePerItem = 0;
+
+            
+            switch (product.ToLower())
+            {
+                case "coffee":
+                    pricePerItem = 1.50;
+                    break;
+                case "water":
+                    pricePerItem = 1.00;
+                    break;
+                case "coke":
+                    pricePerItem = 1.40;
+                    break;
+                case "snacks":
+                    pricePerItem = 2.00;
+                    break;
+                default:
+                    Console.WriteLine("Invalid product");
+                    return 0;
+            }
+
+            
+            return pricePerItem * quantity;
+        }
+    }
+}
